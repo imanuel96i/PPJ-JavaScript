183ad0afe4216bf7b7b4ce154d211bb93aa67389
Manuel Vidal <imanuel96i@gmail.com>
2022-11-15 15:29:27 -0300

    Revisar si el contador es par o impar

--- tienda/src/App.js
+++ tienda/src/App.js
@@ -10,18 +10,29 @@ const App = () => {
   // ! Aumenta el contador (renderiza el app de nuevo)
   const [contador, contadorActualizado] = useState(0)
 
+  const handleClick = () => {
+    contadorActualizado(contador + 1)
+  }
+
+  const handleClickReset = () => {
+    contadorActualizado(0)
+  }
+  //! Revisa si el contador es par o impar
+  const parImpar = contador % 2 === 0
+
   return (
     <div className="App">
       <Mensaje color='red' message='Estamos trabajando' />
       <Mensaje color='green' message='En un curso' />
       <Mensaje color='yellow' message='De React' />
       <Descripcion />
-      <strong>Contador: {contador}</strong>
-      <br/>
-      <button onClick={() => {
-        contadorActualizado(contador + 1)
-      }}>
+      <strong><p>Contador: {contador}</p></strong>
+      <strong><p>{ parImpar ? 'Es par' : 'Es impar'}</p></strong>
+      <button onClick={ handleClick }>
         Incrementar contador
+      </button>
+      <button onClick={ handleClickReset }>
+        Reiniciar contador
       </button>
     </div>
   );
